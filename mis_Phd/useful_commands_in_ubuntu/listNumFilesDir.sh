b25671859488339204a209c37b7c9b3316d8cb43
eriche2016 <1154646635@qq.com>
2016-07-25 09:55:51 +0800

    Update listNumFilesDir.sh

--- mis_Phd/useful_commands_in_ubuntu/listNumFilesDir.sh
+++ mis_Phd/useful_commands_in_ubuntu/listNumFilesDir.sh
@@ -1,1 +1,10 @@
+#choice 1
+# list how many files in current dir(including files in subdir)
 find . -type f | wc -l
+
+#choice 2
+# list how many files in each subdir in current dir
+for i in $(find . -maxdepth 1 -type d) ; do 
+    echo -n $i": " ; 
+    ( find $i -type f | wc -l ) ; 
+done
