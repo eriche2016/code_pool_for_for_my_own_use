90a2643602d7edb1199775a9c97c8b7f8e3926d7
eriche2016 <1154646635@qq.com>
2016-08-13 15:32:46 +0800

    Update list_files_in_dir_store_them_txt.sh

--- mis_Phd/useful_commands_in_ubuntu/list_files_in_dir_store_them_txt.sh
+++ mis_Phd/useful_commands_in_ubuntu/list_files_in_dir_store_them_txt.sh
@@ -1,1 +1,15 @@
+# list files in a directories and store them in a new dir
+# suppose i am in dir coco_dataset/, and there are folders test2014/, val2014/, train2014/
+# and i want to list all the jpg file name in the directory test2014/, val2014/, train2014/ to .txt files 
+ls ./val2014/*.jpg > val2014.txt
+# in val2014.txt:
+# ./val2014/COCO_val2014_000000000042.jpg
+# ./val2014/COCO_val2014_000000000073.jpg
+# ...
 
+ls ./test2014/*.jpg > test2014.txt
+
+# however, when do the operations on train2014, will get the error message:
+#   -bash: /bin/ls: Argument list too long
+# one alternative is to use find below:
+find ./train2014 -maxdepth 1 -name "*.jpg"  > train2014.txt
