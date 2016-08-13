68dfe48cb960810323e86ef97025e96be68f9234
eriche2016 <1154646635@qq.com>
2016-08-13 20:55:49 +0800

    Create sort_file_save_to_another_file.sh

---
+++ mis_Phd/useful_commands_in_ubuntu/sort_file_save_to_another_file.sh
@@ -0,0 +1,5 @@
+# You can use file redirection to redirected the sorted output:
+sort input-file > output_file
+
+# Or you can use the -o option of sort to indicate the same input and output file:
+sort file -o file
