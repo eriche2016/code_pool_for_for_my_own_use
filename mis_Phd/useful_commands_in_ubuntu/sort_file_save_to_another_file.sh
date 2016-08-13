415b95b2baa3d4563f35db41cc3b5bb8e0004125
eriche2016 <1154646635@qq.com>
2016-08-13 21:19:21 +0800

    Update sort_file_save_to_another_file.sh

--- mis_Phd/useful_commands_in_ubuntu/sort_file_save_to_another_file.sh
+++ mis_Phd/useful_commands_in_ubuntu/sort_file_save_to_another_file.sh
@@ -1,5 +1,9 @@
+# by default, sort the file content inlexcographically order
+# you can sort it numerically by using arg -n switch
+
 # You can use file redirection to redirected the sorted output:
 sort input-file > output_file
 
 # Or you can use the -o option of sort to indicate the same input and output file:
 sort file -o file
+
